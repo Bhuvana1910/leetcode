40f42fcea3791c0a9eade30ccbc9a816a44a95ff
Bhuvana1910 <123392576+Bhuvana1910@users.noreply.github.com>
2026-04-25 19:21:09 +0530

    Time: 116 ms (5.03%) | Memory: 103.7 MB (6.15%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        map<int,int>m,n;
+        int d=INT_MIN,f=0;
+        for(int i=0;i<nums.size();i++){
+            n[nums[i]]++;
+            if(n[nums[i]]>f){
+                d=nums[i];
+                f=n[nums[i]];
+            }
+        }
+    
+        int d1=INT_MIN,f1=0;
+        for(int i=0;i<nums.size();i++){
+           m[nums[i]]++;
+           if(m[nums[i]]>f1)
+           {
+            d1=nums[i];
+            f1=m[nums[i]];
+           }
+           
+           if(m.find(d)!=m.end()){
+            if(m[d]>=f1 && (m[d]*2)>(i+1) && ((f-m[d])*2)>(nums.size()-i-1)){
+            return i;
+            }
+            
+           }
+        }
+        return -1;
+    }
+};
