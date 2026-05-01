c3ff4ab1c209ec1e11d4551625b7797433b04bb1
Bhuvana1910 <123392576+Bhuvana1910@users.noreply.github.com>
2026-05-01 16:55:56 +0530

    Time: 1357 ms (10.83%) | Memory: 499.3 MB (79.76%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
     vector<vector<vector<int>>>dp;
-    /*int find(vector<vector<int>>&grid,int i,int j,int k){
-        /*if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size())
-        return -10000;
-        if (k < 0) return -10000;
-        if(dp[i][j][k]!=-100000)
-        return dp[i][j][k];
-        if(i==grid.size()-1 && j==grid[0].size()-1){
-            if(grid[i][j]==0)
-            return grid[i][j];
-            if(k)
-            return grid[i][j];
-            return -10000;
-        }
-        int d=grid[i][j]+find(grid,i+1,j,k-min(1,grid[i][j]));
-        int r=grid[i][j]+find(grid,i,j+1,k-min(1,grid[i][j]));
-        return dp[i][j][k]= max(d,r);
-
-    }*/
     int maxPathScore(vector<vector<int>>& grid, int k) {
         dp.assign(grid.size(), vector<vector<int>>(grid[0].size(), vector<int>(k+1, -100000)));
         int n=grid.size(),m=grid[0].size();
@@ -46,6 +28,6 @@ public:
             }
         }
         return dp[0][0][k]>=0?dp[0][0][k]:-1;
-
+        
     }
 };
