7604ca2e6ac319d7cd13d9b6886ee379d531cc5d
Bhuvana1910 <123392576+Bhuvana1910@users.noreply.github.com>
2026-05-01 16:05:17 +0530

    Time: 868 ms (49.92%) | Memory: 499.3 MB (63.53%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-    int find(vector<vector<int>>& grid,int i,int j, int k){
-        if(k<0)
+    vector<vector<vector<int>>>dp;
+    int find(vector<vector<int>>&grid,int i,int j,int k){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size())
         return -10000;
-        if(i==grid.size()-1 && j==grid[i].size()-1){
-        if(grid[i][j]==0)
-        return grid[i][j];
-        if(k>=1)
-        return grid[i][j];
-        return -10000;
-        }
-        
-        if(i<0 || i>grid.size()-1 || j<0 || j>grid[i].size()-1)
-        return -10000;
-        if(dp[i][j][k]!=-1)
+        if (k < 0) return -10000;
+        if(dp[i][j][k]!=-100000)
         return dp[i][j][k];
-        int right=grid[i][j]+find(grid,i,j+1,k-min(1,grid[i][j]));
-        int down=grid[i][j]+find(grid,i+1,j,k-min(1,grid[i][j]));
-       return dp[i][j][k]=max(right,down);
+        if(i==grid.size()-1 && j==grid[0].size()-1){
+            if(grid[i][j]==0)
+            return grid[i][j];
+            if(k)
+            return grid[i][j];
+            return -10000;
+        }
+        int d=grid[i][j]+find(grid,i+1,j,k-min(1,grid[i][j]));
+        int r=grid[i][j]+find(grid,i,j+1,k-min(1,grid[i][j]));
+        return dp[i][j][k]= max(d,r);
 
     }
     int maxPathScore(vector<vector<int>>& grid, int k) {
-     dp.assign(grid.size(), vector<vector<int>>(grid[0].size(), vector<int>(k+1, -1)));
-     int ans=find(grid,0,0,k);
-     if(ans<0)
-     return -1;
-     return ans;
+        dp.assign(grid.size(), vector<vector<int>>(grid[0].size(), vector<int>(k+1, -100000)));
+        int ans=find(grid,0,0,k);
+        return ans<0 ? -1 : ans;
     }
 };
