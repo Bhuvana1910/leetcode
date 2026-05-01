fdc8a4bd48384abbf3d1a0f024ded94e431c89c0
Bhuvana1910 <123392576+Bhuvana1910@users.noreply.github.com>
2026-05-01 23:35:30 +0530

    Time: 335 ms (5.01%) | Memory: 238 MB (5.00%) - LeetSync

---
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<vector<int>>>dp;
+    int find(string word,int i,int pf1,int pf2){
+        if(i==word.size())
+        return 0;
+        int ch=word[i]-'A'+1;
+        if(dp[i][pf1][pf2]!=-1)
+        return dp[i][pf1][pf2];
+        int f1=0,f2=0;
+        if(i==0){
+            f1=find(word,i+1,ch,pf2);
+            f2=find(word,i+1,pf1,ch);
+        }
+        else{ 
+            if(pf1!=0)
+            f1=abs(m[(pf1-1)+'A'].first-m[word[i]].first)+abs(m[(pf1-1)+'A'].second-m[word[i]].second);
+            f1+=find(word,i+1,ch,pf2);
+            if(pf2!=0)
+            f2=abs(m[(pf2-1)+'A'].first-m[word[i]].first)+abs(m[(pf2-1)+'A'].second-m[word[i]].second);
+            f2+=find(word,i+1,pf1,ch);
+
+        }
+        return dp[i][pf1][pf2]=min(f1,f2);
+
+    }
+    map<char,pair<int,int>>m;
+    int minimumDistance(string word) {
+        int k=0;
+        while(k<26){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<6;j++){
+                m['A'+k]={i,j};
+                k++;
+                if(k>=26)
+                break;
+            }
+        }
+        }
+        dp.assign(word.size(),vector<vector<int>>(27,vector<int>(27,-1)));
+        return find(word,0,0,0);
+    }
+};
