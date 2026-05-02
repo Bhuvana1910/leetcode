2e550fa4de04ce047ba2ae1b56cbf3fe74a8c848
Bhuvana1910 <123392576+Bhuvana1910@users.noreply.github.com>
2026-05-02 12:05:49 +0530

    Time: 517 ms (5.01%) | Memory: 48.6 MB (14.10%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<vector<int>>>dp;
+    /*vector<vector<vector<int>>>dp;
     int find(string word,int i,int pf1,int pf2){
         if(i==word.size())
         return 0;
@@ -23,7 +23,7 @@ public:
         }
         return dp[i][pf1][pf2]=min(f1,f2);
 
-    }
+    }*/
     map<char,pair<int,int>>m;
     int minimumDistance(string word) {
         int k=0;
@@ -37,7 +37,35 @@ public:
             }
         }
         }
-        dp.assign(word.size(),vector<vector<int>>(27,vector<int>(27,-1)));
-        return find(word,0,0,0);
+        /*dp.assign(word.size(),vector<vector<int>>(27,vector<int>(27,-1)));
+        return find(word,0,0,0);*/
+        vector<vector<vector<int>>>dp(word.size(),vector<vector<int>>(27,vector<int>(27,1000000)));
+        for(int i=word.size()-1;i>=0;i--){
+            for(int j=0;j<27;j++){
+                for(int k=0;k<27;k++){
+                    int f1=0,f2=0;
+                    int ch=word[i]-'A'+1;
+                    if(i==0){
+                    f1=dp[i+1][ch][k];
+                    f2=dp[i+1][j][ch];
+                    }
+                    else{ 
+            if(j!=0)
+            f1=abs(m[(j-1)+'A'].first-m[word[i]].first)+abs(m[(j-1)+'A'].second-m[word[i]].second);
+            if(i+1<word.size())
+            f1+=dp[i+1][ch][k];
+            if(k!=0)
+            f2=abs(m[(k-1)+'A'].first-m[word[i]].first)+abs(m[(k-1)+'A'].second-m[word[i]].second);
+            if(i+1<word.size())
+            f2+=dp[i+1][j][ch];
+        }
+        dp[i][j][k]=min(f1,f2);
+
+                }
+            }
+        }
+        return dp[0][0][0];
+
+
     }
 };
