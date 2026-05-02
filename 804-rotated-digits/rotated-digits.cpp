9b019d1f2c20d0885b141c49a44a50a956c23560
Bhuvana1910 <123392576+Bhuvana1910@users.noreply.github.com>
2026-05-02 14:38:37 +0530

    Time: 3 ms (33.51%) | Memory: 9.7 MB (18.77%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    vector<int>dp;
     int check(int i){
         if(i==0)
         return 0;
+        if(dp[i]!=-1)
+        return dp[i];
         int r=check(i/10);
         if(r==2)
         return 2;
@@ -13,14 +16,15 @@ public:
         else if(d==2 || d==5 || d==6 || d==9)
         v_d=1;
         else
-        return 2;
+        return dp[i]=2;
         if(r==0 && v_d==0)
-        return 0;
-        return 1;
+        return dp[i]=0;
+        return dp[i]=1;
 
 
     }
     int rotatedDigits(int n) {
+        dp.assign(n+1,-1);
         int c=0;
         for(int i=1;i<=n;i++){
             
