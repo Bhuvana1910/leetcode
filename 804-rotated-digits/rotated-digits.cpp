e0e31208f698d556362d2410254fd4ec943fe1cd
Bhuvana1910 <123392576+Bhuvana1910@users.noreply.github.com>
2026-05-02 16:27:47 +0530

    Time: 2 ms (40.88%) | Memory: 8.3 MB (36.67%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
+    vector<vector<vector<int>>>dp;
     int rd(string s, int i, bool tight, bool isval) {
     if (i == s.size()) {
         return isval ? 1 : 0;
     }
-
+    if(dp[i][tight][isval]!=-1)
+    return dp[i][tight][isval];
     int ub = (tight ? s[i] - '0' : 9);
     int res = 0;
-
     for (int d = 0; d <= ub; d++) {
 
         if (d == 3 || d == 4 || d == 7)
@@ -20,11 +21,11 @@ public:
 
         res += rd(s, i + 1, (tight && d == ub), f);
     }
-
-    return res;
+    return dp[i][tight][isval]=res;
 }
     int rotatedDigits(int n) {
         string s=to_string(n);
+        dp.assign(s.size(),vector<vector<int>>(2,vector<int>(2,-1)));
         return rd(s,0,1,false);
     }
 };
