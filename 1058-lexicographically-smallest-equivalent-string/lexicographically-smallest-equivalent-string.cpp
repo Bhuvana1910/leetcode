95dd892cdb058fbbe49eb8781583471900bb4ba8
Bhuvana1910 <123392576+Bhuvana1910@users.noreply.github.com>
2026-05-05 14:20:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (88.24%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    char dfs(vector<vector<char>>&graph,int c){
-        char t=c+'a';
-        deque<int>q;
-        set<int>vis;
-        q.push_back(c);
-        vis.insert(c);
-        while(!q.empty()){
-            char s=q.front();
-            t=min(t,(char)(q.front()+'a'));
-            q.pop_front();
-            for(int i=0;i<graph[s].size();i++){
-                if(!vis.count(graph[s][i]))
-                {
-                    vis.insert(graph[s][i]);
-                    q.push_back(graph[s][i]);
-                }
-            }
-        }
-        return t;
+    vector<int>parent;
+    int findparent(int u){
+        if(parent[u]==u)
+        return u;
+        return parent[u]=findparent(parent[u]);
+    }
+    void unions(char s1,char s2){
+        int x=findparent(s1-'a');
+        int y=findparent(s2-'a');
+        if(x<y)
+        parent[y]=x;
+        else
+        parent[x]=y;
+       
     }
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        vector<vector<char>>graph(26);
-        for(int i=0;i<s1.size();i++){
-            int u = s1[i] - 'a';
-            int v = s2[i] - 'a';
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-        }
+        parent.assign(26,0);
+        for(int i=0;i<26;i++)
+        parent[i]=i;
+        for(int i=0;i<s1.size();i++)
+        unions(s1[i],s2[i]);
+        for(int i=0;i<26;i++)
+        findparent(i);
+
         for(int i=0;i<baseStr.size();i++){
-            baseStr[i]=dfs(graph,baseStr[i]-'a');
+            baseStr[i]= baseStr[i] = (char)(findparent(baseStr[i] - 'a') + 'a');
+
         }
         return baseStr;
     }
