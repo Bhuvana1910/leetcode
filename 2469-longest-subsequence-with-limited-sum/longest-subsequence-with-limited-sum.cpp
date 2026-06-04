f67f827539ba4ab516a84cf219a82c12f56b185c
Bhuvana1910 <123392576+Bhuvana1910@users.noreply.github.com>
2026-06-04 20:06:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (68.52%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
-    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
-      sort(nums.begin(),nums.end());
-      vector<int>v(queries.size(),0);
-      for(int i=0;i<queries.size();i++)
-      { int ans=0;
-        for(int j=0;j<nums.size();j++)
-        
-        {ans+=nums[j];
-         if(queries[i]>=ans)
-         v[i]=j+1;
+int check(int l,int h,vector<int> &v,int i,vector<int>& queries)
+{int ans=0;
+    while(l<=h)
+    {
+        int mid=(l+h)/2;
+        if(queries[i]>=v[mid])
+        {
+        l=mid+1;
+        ans=mid+1;
         }
-      } 
-      return v; 
+        else 
+        h=mid-1;
+    }
+    return ans;
+}
+
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(),nums.end());
+        vector<int>v(nums.size());
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+           sum+=nums[i];
+           v[i]=sum;
+        }
+        vector<int>a(queries.size(),0);
+        int l=0;
+        int h=v.size()-1;
+        for(int i=0;i<queries.size();i++)
+        {
+        int c=check(l,h,v,i,queries);
+        a[i]=c;
+        }
+        return a;
     }
 };
